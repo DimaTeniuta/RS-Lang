ef23b17d76a4053b273402e9b83b9ecc20fa7c0f
dimateniuta <dteniuta@gmail.com>
2022-10-17 22:13:26 +0300

    feat: add files for deploy

--- src/components/variables/word.variables.ts
+++
@@ -1,9 +0,0 @@
-export const WORDS_PER_PAGE = 20;
-
-export const PAGES_PER_GROUP = 30;
-
-export const SERIA_FOR_NORMAL = 3;
-
-export const SERIA_FOR_HARD = 5;
-
-export const AUDIOCALL_ANSWER_NUMBER = 5;
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import './style.scss';
-import app from './components/app/app';
-
-app.start();
